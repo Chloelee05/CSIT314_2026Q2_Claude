a95e01fb8f7c5c05e9a9b3066413518d76f83a27
Nadon <150321839+nadon-pan@users.noreply.github.com>
2026-05-15 15:17:23 +0800

    reimplement: user story #16

--- lib/controllers/LoginController.ts
+++ lib/controllers/LoginController.ts
@@ -31,7 +31,7 @@ export class LoginController {
     );
 
     if (!success || !user) {
-      return [false, message];
+      return [false, 'Unsuccessful Login.'];
     }
 
     await createSession({
